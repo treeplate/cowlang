63b87199e8a7251994c7796c8a282ae22daac3fd
treeplate <tree.plate.moo@gmail.com>
2025-02-18 14:07:29 -0800

    add some rtl functions

--- rtl.dart
+++ rtl.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'dart:io';
+
 import 'ast.dart';
 import 'lexer.dart';
 
@@ -5,9 +8,11 @@ Scope rtl = Scope(null, 'rtl', IdentifierToken('import', 0, 0, 'rtl.dart'), [
   'rtl (./rtl.dart:1:1)',
 ])..variables.addAll(rtlVariables);
 
+final Random random = Random();
+
 final Map<String, Object?> rtlVariables = {
   'print': (List<Object?> args, List<String> stackTrace) {
-    stackTrace.add('print (./rtl.dart:10:1)');
+    stackTrace.add('print (./rtl.dart:15:1)');
     if (args.length != 1) {
       throwRuntimeException(
         'error: print expects 1 argument, got ${args.length}',
@@ -28,7 +33,7 @@ final Map<String, Object?> rtlVariables = {
     stackTrace.removeLast();
   },
   'intToStr': (List<Object?> args, List<String> stackTrace) { // maybe implement in .cow file?
-    stackTrace.add('intToStr (./rtl.dart:31:1)');
+    stackTrace.add('intToStr (./rtl.dart:36:1)');
     if (args.length != 1) {
       throwRuntimeException(
         'error: intToStr expects 1 argument, got ${args.length}',
@@ -37,9 +42,71 @@ final Map<String, Object?> rtlVariables = {
     }
     Object? arg = args.single;
     if (arg is! int) {
-      throwRuntimeException('error: intToStr expects list, got $arg', stackTrace);
+      throwRuntimeException('error: intToStr expects int, got $arg', stackTrace);
     }
     stackTrace.removeLast();
     return arg.toString().runes.toList();
+  },
+  'random': (List<Object?> args, List<String> stackTrace) {
+    stackTrace.add('random (./rtl.dart:51:1)');
+    if (args.length != 1) {
+      throwRuntimeException(
+        'error: intToStr expects 1 argument, got ${args.length}',
+        stackTrace,
+      );
+    }
+    Object? arg = args.single;
+    if (arg is! int) {
+      throwRuntimeException('error: random expects int, got $arg', stackTrace);
+    }
+    stackTrace.removeLast();
+    return random.nextInt(arg);
+  },
+  'input': (List<Object?> args, List<String> stackTrace) {
+    stackTrace.add('input (./rtl.dart:66:1)');
+    if (args.length != 0) {
+      throwRuntimeException(
+        'error: input expects 0 arguments, got ${args.length}',
+        stackTrace,
+      );
+    }
+    stackTrace.removeLast();
+    return stdin.readLineSync()!.runes.toList();
+  },
+  'strToInt': (List<Object?> args, List<String> stackTrace) { // maybe implement in .cow file?
+    stackTrace.add('strToInt (./rtl.dart:77:1)');
+    if (args.length != 1) {
+      throwRuntimeException(
+        'error: strToInt expects 1 argument, got ${args.length}',
+        stackTrace,
+      );
+    }
+    Object? arg = args.single;
+    if (arg is! List) {
+      throwRuntimeException('error: strToInt expects list, got $arg', stackTrace);
+    }
+    if (arg.any((e) => e is! int)) {
+      throwRuntimeException(
+        'error: strToInt expects list of numbers, got $arg (containing a non-number)',
+        stackTrace,
+      );
+    }
+    stackTrace.removeLast();
+    return int.parse(String.fromCharCodes(arg.cast()));
+  },
+  'positive': (List<Object?> args, List<String> stackTrace) { // maybe implement in .cow file?
+    stackTrace.add('positive (./rtl.dart:98:1)');
+    if (args.length != 1) {
+      throwRuntimeException(
+        'error: positive expects 1 argument, got ${args.length}',
+        stackTrace,
+      );
+    }
+    Object? arg = args.single;
+    if (arg is! int) {
+      throwRuntimeException('error: positive expects int, got $arg', stackTrace);
+    }
+    stackTrace.removeLast();
+    return arg > 0 ? 0 : 1;
   },
 };
